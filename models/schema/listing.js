6449f7069792505b5bcd64dd059c878426a26c45
Andrew Vittiglio <andrew@Andrews-MacBook-Air.local>
2022-06-28 23:32:30 -0400

    added listing schema

--- models/schema/listing.js
+++ models/schema/listing.js
@@ -1,1 +1,49 @@
 //an object of a listing that a user will send to the website
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; 
+
+var userSchema = new Schema ({
+	//define what the data inputs should be here (aka the Schema)!
+	id: {type: Number, required: true}, 
+	name: {type: String, required: true}, 
+	description: String,
+	url: {type: String, required: true}
+	price: {type: Number, required: true},
+	tags: [String],
+	postedBy: {type: Schema.ObjectId, required: true}, 
+	imageURL: String, 
+	condition; String, 
+	approvedDate: Date, 
+	availability:  Boolean
+	}, 
+	{
+        toObject: { getters: true },
+        timestamps: {
+            createdAt: 'createdDate',
+            updatedAt: 'updatedDate'
+        },
+    }
+
+);
+
+userSchema.pre('save', function(callback){
+	//run hook code
+ 	if(!this.availability){
+
+	this.availability = true;  
+
+	}
+
+	callback();
+});
+
+//create any methods
+userSchema.methods.greet = function(){
+	console.log('hi');
+
+};
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User; 
--- models/schema/users.js
+++ models/schema/users.js
@@ -3,10 +3,24 @@ const Schema = mongoose.Schema;
 
 var userSchema = new Schema ({
 	//define what the data inputs should be here (aka the Schema)!
-
+	firstName: String, 
+	lastName: String, 
+	university: String, 
+	address: String, 
+	classYear: Number, 
+	email: String,
+	phoneNumber: String, //String b/c Number ignores any leading zeros 
+	phoneProvider: String, 
+	venmo: String , 
+	isAdmin: Boolean,  
+	isSuperAdmin: Boolean, 
+	hash: String, 
+	creationDate: Date
 	}, 
 	{
         toObject: { getters: true },
+
+	//changes the names of mongoose defualt timestamps -- easier to read/remember
         timestamps: {
             createdAt: 'createdDate',
             updatedAt: 'updatedDate'
@@ -15,19 +29,45 @@ var userSchema = new Schema ({
 
 );
 
-
+//runs code before we save 
 userSchema.pre('save', function(callback){
 	//run hook code
- 
-	callback();
+ 	if(this.isAdmin){ 
+		if(!this. hash && !this.password){
+			throw new Error("Password required for Admnins");
+		}
+		this.hash = this.hash || this.password;
+	
+	// TODO hash the password 
+	
+	} 
+	//non-admin requirements
+	else{
+		if (!this.phone){
+			throw new Error('Missing Phone');
+		}
+		
+		// TODO check that phone number is real 
+
+        	if (!this.phoneProvider){
+			throw new Error ('Missing Phone Provider');
+		}	
+	}
+
+	callback() 
 });
 
 
 //create any methods
 userSchema.methods.greet = function(){
 	console.log('hi');
-
 };
+
+// TODO create method to check hashed password
+
+userSchema.methods.checkPassword = function(pw){
+	return  this.hash===pw; 
+}
 
 var User = mongoose.model('User', userSchema);
 
