3d53f441aa73b34a59d38432805886311303fea3
Andrew Vittiglio <andrew@Andrews-MacBook-Air.local>
2022-06-29 13:39:23 -0400

    added basic user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,55 @@
+var userDb = [];
+var idCounter = 0;
+
+userDb.insertUser = function(user, callback){
+	user.id = idCounter;
+	idCounter++;
+	userDb.push(user); 
+	
+	callback();
+
+}; 
+
+userDb.getUserById = function(id, callback){
+
+	for(var i=0, i<this.length, i++){
+		if(this[i].id === + id){
+			callback(this[i])
+		}
+	}
+};
+
+
+
+
 module.exports.createUser = function(req, res){
 	
-	res.send('User has been created');
-	console.log("A User Creation Request")
+	var newUser = {
+	firstName: req.body.firstName, 
+	lastName: req.body.lastName, 
+	email: req.body.email,
+	password: req.body.password, 
+	createdDate: new Date()
+
+	}
 	
+	//stores user in userDb
+	userDb.insertUser(newUser, function(){
+		return res.send("New User Created!");
+	});
+
+};
+
+module.exports.getUsers = function(req, res){
+
+	return res.send(userDb);
+
+};
+
+
+module.exports.getUserById = function(req, res){
+userDb.getUserById(req.params.id, function(user){
+		return res.json(user);
+	});
+
 }
-
-
-
-
-
