792d515bbfe7ca5de7c30b4f14c0dfc41960524d
Andrew Vittiglio <andrew@Andrews-MacBook-Air.local>
2022-07-06 12:43:28 -0400

    update user.js controller methods

--- controllers/users.js
+++ controllers/users.js
@@ -1,55 +1,54 @@
-var userDb = [];
-var idCounter = 0;
+const User = require('../models/schemas/user');
 
-userDb.insertUser = function(user, callback){
-	user.id = idCounter;
-	idCounter++;
-	userDb.push(user); 
-	
-	callback();
+module.exports.createUser = function(req, res) {
+    // validate input
 
-}; 
+    // create actual user
+    var data = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        createdDate: new Date()
+    };
 
-userDb.getUserById = function(id, callback){
+    var newUser = new User(data);
 
-	for(var i=0, i<this.length, i++){
-		if(this[i].id === + id){
-			callback(this[i])
-		}
-	}
-};
+    newUser.save(function(err, user) {
+        return res.send('it worked!!!');
+    });
+}
 
+module.exports.getUsers = function(req, res, next) {
+    User.find({}, function(uhoh, anarrayofusers) {
+        if (uhoh) return next(uhoh);
+        return res.json(anarrayofusers);
+    });
+}
 
+module.exports.getUserById = function(req, res, next) {
+    User.findById(req.params.id, function(err, user) {
+        if (err) return next(err);
+        if (!user)
+            return res.status(404).send('user not found');
+        return res.json(user);
+    });
+}
 
+module.exports.updateUser = function(req, res, next) {
+    User.findOneAndUpdate(req.params.id, req.body,
+            { new: true }, function(err, user) {
+        if (err) return next(err);
+        if (!user) return res.status(404).send('No user with that ID');
+        return res.json(user);
+    });
+}
 
-module.exports.createUser = function(req, res){
-	
-	var newUser = {
-	firstName: req.body.firstName, 
-	lastName: req.body.lastName, 
-	email: req.body.email,
-	password: req.body.password, 
-	createdDate: new Date()
-
-	}
-	
-	//stores user in userDb
-	userDb.insertUser(newUser, function(){
-		return res.send("New User Created!");
-	});
-
-};
-
-module.exports.getUsers = function(req, res){
-
-	return res.send(userDb);
-
-};
-
-
-module.exports.getUserById = function(req, res){
-userDb.getUserById(req.params.id, function(user){
-		return res.json(user);
-	});
-
+module.exports.deleteUserById = function(req, res, next) {
+    User.findOneAndRemove(req.params.id, function(err, user) {
+        if (err) return next(err);
+        if (!user)
+            return res.status(404).send('No user with that ID');
+        return res.sendStatus(200);
+    });
 }
